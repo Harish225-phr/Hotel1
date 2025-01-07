fff234b5ff4823a807aaf197ddbab6b305c48f9f
harish <harish.rajjput@gmail.com>
2025-01-08 00:59:45 +0530

    navbar fixes done

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
             <i className="ri-menu-line"></i>
           </div>
         </div>
-        <ul  className={`nav__links ${isMenuOpen ? 'show' : ''}`} id="nav-links">
+        <ul   className={`nav__links ${isMenuOpen ? 'open' : ''}`} id="nav-links">
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#room">Room</a></li>
